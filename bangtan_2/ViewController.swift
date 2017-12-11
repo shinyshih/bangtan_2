9d32d45be6f0d126b769b200cff5c7c5a517caf7
施馨檸 <shiny761118@gmail.com>
2017-12-11 10:10:07 +0800

    Initial Commit

---
+++ bangtan_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bangtan_2
+//
+//  Created by 施馨檸 on 11/12/2017.
+//  Copyright © 2017 施馨檸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
